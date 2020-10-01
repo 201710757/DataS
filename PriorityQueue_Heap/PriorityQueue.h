5223ebc9f638af4b42496d46373e4101d641d792
201710757 <neti69@naver.com>
2020-10-01 22:24:35 +0900

    Priority

---
+++ PriorityQueue_Heap/PriorityQueue.h
@@ -0,0 +1,21 @@
+#ifndef __PRIORITY_QUEUE_H__
+#define __PRIORITY_QUEUE_H__
+
+#include "UsefulHeap.h"
+
+typedef Heap PQueue;
+typedef HData PQData;
+
+class PriQue
+{
+    public:
+        PQueue * ppq;
+        
+        PriQue(PriorityComp pc);
+        int PQIsEmpty();
+        void PEnqueue(PQData data);
+        PQData PDequeue();
+};
+
+
+#endif
